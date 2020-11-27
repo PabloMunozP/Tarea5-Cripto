d81ebbcc3eb4b01edc57396304f479ee9ad6f63f
PabloMunozP <pablo.munoz@2mail.udp.cl>
2020-11-26 21:54:24 -0300

    update

--- Tarea.py
+++ Tarea.py
@@ -1,78 +1,76 @@
-import imaplib,email,re,smtplib
+import imaplib,email,re,smtplib,csv
+from getpass import getpass
 from email.message import EmailMessage
-
-correo_personal= 'frosty98.pmo@gmail.com'
-password='tzsvnonchwmkmajs'
-correo_testeo='noreply@redditmail.com '
-
-
-def get_IDs(correo,password):
-    imap = imaplib.IMAP4_SSL('imap.gmail.com')
-    imap.login(correo,password)
-    messages_count = imap.select('INBOX')[1]
-    status,mails_id=imap.search(None,'(FROM "noreply@redditmail.com")')
-    mails_id=str(mails_id)[3:-2].rsplit(' ')
-    outFile=open('Message-IDs.txt','w')
-    regex=[]
-    for id in mails_id:
-        print(id)
-        res,msg = imap.fetch(id,'(RFC822)')
-        for msg_data in msg:
-            if isinstance(msg_data,tuple):
-                msg_= email.message_from_bytes(msg_data[1])
-                print(msg_['Message-ID'],'\n')
-                regex.append((id,msg_['Message-ID'][1:-1]))
-                outFile.write(msg_['Message-ID'][1:-1]+'\n')
-    outFile.close()
-    return regex
-
-def get_regex(FileName):
-    inFile = open(FileName,'r')
-    line = inFile.readline()
-    line = line.rsplit(',')
-    return line
-    
-def send_mail():
-   pass
+from email.parser import HeaderParser
+from time import sleep
+from dateutil import parser
 
 def menu():
     print(''' Tarea 5 Pablo Muñoz Poblete
-            1- Obtener los Message IDs.
-            2- Comprobar IDs con regex importada.
-            3- Suplantar el correo 
-            4- Revisar alertas
-            5- Salir
+            1-Importar Regex.
+            2-Conseguir Message-ID.
+            3-Validar correos.
+            4- Salir
         ''')
+
+def import_regex(regex_file):
+    try:
+        regex_list=[]
+        with open(regex_file,'r') as file:
+            reader=csv.reader(file)
+            for line in reader:
+                regex_list.append(line)
+        #print(regex_list)
+        return regex_list
+    except Exception as e:
+        print('Error: ',e)
+
+def connect_mail(user,pwd):
+    try:
+        imap=imaplib.IMAP4_SSL('imap.gmail.com')
+        imap.login(user,pwd)
+        print('Se utilizara la bandeja de entrada para la revision del correo.')
+        msg_count=imap.select('INBOX')[1][0]
+        print('Se encontraron ',msg_count.decode('utf-8'), ' mensajes en la bandeja de entrada.')
+        return imap
+    except Exception as e:
+        print('Error: ', e)
+    
+
+
 
 if __name__ == "__main__":
     try:
-        while True:
-            menu()
-            op=input('Ingresa la opcion deseada: ') 
-            while op not in ['1','2','3','4','5']:
-                op=input('Ingresa la opcion deseada: ')
-            if op == '1':
-                print('El correo utilizado es: ',correo_personal,' y se revisaran los correos provenientes de: ',correo_testeo )
-                msg_ids=get_IDs(correo_personal,password)
-            if op == '2':
-                if not msg_ids:
-                    print('Debe obtener los Message-ID primero')
-                else:
-                    regex=get_regex('regex.txt')#regex,correo,fecha
-                    print('Chequeando si los Message-ID corresponden con el regex entregado...')
-                    for id in msg_ids:
-                        #id, msg_id
-                        match=re.match('r'+regex,id[1])
-                        if match:#el msg_id es correcto
-                            print('El correo cumple con la regex y su Message_ID es:',id[1])
+        regex_file=input('Ingrese el nombre del archivo a importar.')
+        # regex_file='regex.csv'
+        regex_list=import_regex(regex_file)
+        user=input('Ingrese el correo a revisar: ')
+        pwd=getpass('Ingrese la contraseña: ')
+        client=connect_mail(user,pwd)
+        for regex in regex_list:
+            status,mails_id=client.search(None,'(FROM "' +regex[0]+'")')
+            regex_date=parser.parse(regex[2]).date()
+            print(regex_date)
+            if status != 'OK':
+                print('El correo',regex[0], 'no esta disponible')
+                continue
+            mails_id=mails_id[0].decode('utf-8').rsplit(' ')
+            print('--------Comprobando el correo: ',regex[0],', con la regex: ',regex[1],'---------------------')
+            for id in mails_id:
+                res,msg = client.fetch(id,'(BODY[HEADER])')
+                for msg_data in msg:
+                    if isinstance(msg_data,tuple):
+                        msg_content=email.message_from_bytes(msg_data[1])
+                        print('Comprobando el mensaje',id,'con la regex',regex[1],'y la expresion',msg_content['Message-ID'][1:-1],'...')
+                        match=re.match(regex[1],msg_content['Message-ID'][1:-1])
+                        if match:#el mensaje es autentico
+                        print('El correo es autentico')
                         else:
-                            print('El correo no cumple con la regex y su Message_ID es: ',id[1])
-            if op == '3':
-                send_mail()
-            if op == '4':
-                pass 
-            if op == '5':
-                exit()       
-    except KeyboardInterrupt :
-        exit()
-    
+                            mail_date=parser.parse(msg_content['Date']).date()
+                            if regex_date > mail_date:
+                                print(regex_date,mail_date)
+                                print('La fecha del mensaje es anterior a la registrada, es posible que la regex no corresponda ')
+                            print('El correo podria ser falso')
+            sleep(1)           
+    except Exception as e:
+        print('Error: ',e)
